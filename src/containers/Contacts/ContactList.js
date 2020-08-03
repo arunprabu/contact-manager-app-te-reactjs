96eefcd4fd89c83107b4f65ed42b1401379abbdd
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-08-03 13:20:34 +0530

    updated

--- src/containers/Contacts/ContactList.js
+++ src/containers/Contacts/ContactList.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ContactList() {
   return (
-    <div>
+    <div className='text-left'>
       <h2>Contact List</h2>
-      Loop thru multiple Contact Component 
+
+      <div className="list-group">
+        <div className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">
+              <Link to={`/contacts/1`}>John</Link>
+            </h5>
+            <small>Contact Id: 1</small>
+          </div>
+          <p className="mb-1">
+            John is from NZ
+          </p>
+        </div>
+      </div>
+      
     </div>
   )
 }
