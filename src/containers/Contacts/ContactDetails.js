f638aac58050254b76511c88ac1704e5f85d38c3
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-08-03 13:38:03 +0530

    updated

--- src/containers/Contacts/ContactDetails.js
+++ src/containers/Contacts/ContactDetails.js
@@ -16,9 +16,50 @@ class ContactDetails extends Component {
                 John is from NZ
               </p>
               <br />
-              <button className='btn btn-primary' data-toggle='modal' 
-              data-target='#editModal'>Edit</button> &nbsp;
+              <button className='btn btn-primary' data-toggle='modal'
+                data-target='#editModal'>Edit</button> &nbsp;
               <button className='btn btn-danger'>Delete</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="modal fade" id="editModal"
+          tabIndex={-1}
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Update Contact
+                  </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <input required type="text"
+                    placeholder="Enter Name"
+                    className='form-control'
+                  /><br />
+                  <input required type="text"
+                    placeholder="Enter E-Mail"
+                    className='form-control'
+                  /><br />
+                  <input required type="text"
+                    placeholder="Enter Phone"
+                    className='form-control'
+                  /><br />
+                  <button className='btn btn-primary'
+                    type='submit'>Save Changes</button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
